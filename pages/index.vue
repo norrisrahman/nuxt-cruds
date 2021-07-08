1871fba4378fc6870b663c7733a9a053915e1bd2
norrisrahman <norisdr8@gmail.com>
2021-07-08 13:12:03 +0700

    Main

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="wrapper">
+    <app-sidebar></app-sidebar>
+    <div id="content-wrapper">
+      <div id="content">
+        <app-header></app-header>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '../components/layouts/sidebar.vue'
+import Header from '../components/layouts/header.vue'
+
+export default {
+  components : {
+    'app-sidebar': Sidebar,
+    'app-header' : Header
+  }
+
+}
+</script>
